52829e61879527f2b4c94cf97463138a237fe881
glapo992 <108743608+glapo992@users.noreply.github.com>
2022-11-23 20:04:59 +0100

    corretto api e gps reader, creati file.txt con risultati per facilitare lo sviluppo

--- backendFlask/test_main.py
+++ backendFlask/test_main.py
@@ -1,25 +1,36 @@
 from plantnet import PlantNet as pt
-import json
-from jpg_image_conversion import ConvertImg as ci
+from exifManager import ExifManager as em
 
-FILEPATH = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/src/identification.txt'
 #---------FILE TEMPORANEO PER PROVARE API ------------
 '''lista di 5 imamgini locali per vedere se la api funziona
-la lista verrà compilata con le immagini fornite dal form
+la lista verrà compilata con le immagini fornite dal form html
 '''
+image1 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img1.jpg'
+image2 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img2.jpg'
+image3 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img3.jpg'
+image4 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img4.jpg'
 
-image1 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/salvia.jpg'
-image2 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/salvia1.jpg'
-image3 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/salvia2.jpg'
-image4 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/salvia3.jpg'
+imagesList = [image1, image2, image3, image4]
 
-images_salvia = [image1, image2, image3, image4]
+#lettura metadati GPS
+openImg = em.img_opener(imagesList[0])
+gpsInfo = em.exif_to_tag(openImg)
 
-#prima di passare le immagini alla api le converto in jpg
-files = pt.readImg(images_salvia)
+#prima di passare le immagini alla api vanno convertite in jpg
 
+#invio ed identificazione immagini
+files = pt.readImg(imagesList)
 result = pt.sendImg(files)
 
-#teporaneo, salva il result su file txt per sviluppo. cancellare per prima del deploy
-with open(FILEPATH, 'w') as outfile:
-    json.dump(result, outfile)
+print(result)
+print(gpsInfo)
+'''
+#teporaneo, salva il result su file txt per SVILUPPO. cancellare per prima del deploy
+with open('/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/src/identification.txt', 'w') as outfile:
+     json.dump(result, outfile)
+
+with open(
+        '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/src/gpsSample.txt',
+        'w') as convert_file:
+    convert_file.write(json.dumps(gpsInfo))
+'''
