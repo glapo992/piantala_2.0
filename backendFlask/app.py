0c1ab2ff06b0f08cc5be9696e59034fa9a4b4139
glapo992 <108743608+glapo992@users.noreply.github.com>
2022-11-24 20:29:26 +0100

    centrato elementi frontend e corretto i link con flask

--- backendFlask/app.py
+++ backendFlask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from titolo import Titolo
 from plantnet import *
 from Esegui import esegui
@@ -9,19 +9,15 @@ app = Flask(__name__)
 #il render_template apre il file all'interno della cartella 'templates'
 @app.route('/')
 def index():
-    #titolo dinamico fatto solo per vedere come funziona jinja e oggetti in py
-    landing1 = Titolo('piantala', 'di rompere il cazzo')
-
-    return render_template(
-        'index.html',
-        #rimuovere questa parte sotto e il landing1
-        titolo_princ=landing1.primo_titolo,
-        sottotitolo=landing1.sottotitolo,
-    )
+    return render_template('index.html')
 
 
-#reindirizzamento alla pagina aboutus
-@app.route('/about/')
+@app.route('/progetto1')
+def progetto1():
+    return render_template('progetto1.html')
+
+
+@app.route('/about')
 def about():
     image1 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img1.jpg'
     image2 = '/Users/giuliolapovich/Code/aws-azure/piantala/backendFlask/static/img/piante/img2.jpg'
