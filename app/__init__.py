2917dc99cb50f0e50bd147303b0c37d977b5d11d
glapo992 <giuliolapovich@yahoo.it>
2023-06-23 13:54:24 +0200

    form saves images

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from config import Config
+#from flask_uploads import configure_uploads, IMAGES, UploadSet
 
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -19,7 +20,6 @@ def create_app(config_class = Config):
     """
     app = Flask(__name__)
     app.config.from_object(config_class)
-
     db.init_app(app)
     migrate.init_app(app, db, render_as_batch=True)
 
