217b12251819c12817573e4e53c2c0ae18ecfe33
glapo992 <giuliolapovich@yahoo.it>
2023-06-26 16:25:19 +0200

    add result page

--- utils/plantnet.py
+++ utils/plantnet.py
@@ -9,13 +9,15 @@ from app import db
 # for debug only -> send a static json so the api is not called every time
 from identification_sample import IDENT_FULL, IDENT_PART
 
-def manage_plant_form(form):
-    """ Manages the form result and write on database
+def manage_plant_form(form)-> Identification_mini:
+    """ Manages the form result and writes on database
 
     :param form: form plant upload
     :type form: FlaskForm
     :return: response view
     :rtype: view
+    :return: identified plant object
+    :rtype: Identification_mini
     """
     filename = form.upload(Config.UPLOAD_FOLDER) # save files in temp folder
     # list of paths of images for the API
@@ -49,6 +51,7 @@ def manage_plant_form(form):
 
     clearfolder(Config.UPLOAD_FOLDER) #clear temp folder
     clearfolder(Config.CONVERTED_FOLDER) #clear temp folder
+    return identfiy
 
 
 def sendImg(files:list[str], organs)->list:
@@ -63,7 +66,7 @@ def sendImg(files:list[str], organs)->list:
     req = requests.Request('POST', url=Config.API_ENDPOINT, files=files, data=data)
     print(data)
 
-    #! imported static json just for testing
+    #! imported static json just for testing so the api is not called every time
     #prepared = req.prepare()
     #s = requests.Session()
     #response = s.send(prepared)
@@ -85,7 +88,7 @@ def plant_json_to_list(json_result:json)-> list[str]:
     :return: list with used datas
     :rtype: list[str]
     """
-    # Prova a inserire ogni valore, altrimenti usa un valore di default
+    # tries to insert the value, else fills with null
     if json_result['results']:
         print('full ans')
         try:
