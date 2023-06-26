ef9d855c1a600ea35043d8fa229c6df3e6721dbd
glapo992 <giuliolapovich@yahoo.it>
2023-06-26 16:25:19 +0200

    add result page

--- app/plantnet/routes.py
+++ app/plantnet/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from flask import render_template, flash, redirect, url_for
+from app.models import Identification_mini
 from app.plantnet import bp
 from app.plantnet.forms import ImageForm
 from utils.plantnet import manage_plant_form
@@ -7,37 +8,23 @@ from utils.plantnet import manage_plant_form
 
 import os
 
-
-organs =[]
-
 @bp.route('/', methods = ['GET', 'POST'])
 def index():   
     form = ImageForm()
     if form.validate_on_submit():
-        manage_plant_form(form=form)
+        plant = manage_plant_form(form=form)
         flash('File caricati!')
-        return redirect(url_for('plantnet.index')) #TODO redirect to result page
+ 
+        return redirect(url_for('plantnet.response', plant_id = plant.id))
     return render_template('index.html' , title='Home', form = form)
 
+@bp.route('/response/<plant_id>')
+def response(plant_id):
+    plant = Identification_mini.query.filter_by(id=plant_id).first()
+    return render_template ('response.html', plant = plant)
 
 
-@bp.route('/mappa')
-def mappa():
-    return render_template('mappa.html',title='Mappa')
 
-
-@bp.route('/circle_map')
-def circle_map():
-    # Rigenera l'html della mappa
-    try:
-        return render_template('circle_map.html')
-    except:
-        return redirect('errors.404', title='404')
-
-
-@bp.route('/response')
-def response():
-    return None
 """
 
     #------------mappa---------------------------------------------------------------
@@ -51,10 +38,7 @@ def response():
                     m=dv.mappa(Config.JSON_FOLDER + 'data.json'),
                     specie=risposta[0]))
 
-    # Cancella contenuto nelle cartelle tmp
-    clearfolder(Config.JSON_FOLDER)
-    clearfolder(Config.UPLOAD_FOLDER)
-    clearfolder(Config.CONVERTED_FOLDER)
+
     return render_template('response.html', risposta=risposta, tagGPS=tagGPS)"""
     
 
