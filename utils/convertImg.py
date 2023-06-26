a54b11681ae6ae73e35b00ae01a229003c84c76b
glapo992 <giuliolapovich@yahoo.it>
2023-06-26 14:05:39 +0200

    add conv img to jpg

--- utils/convertImg.py
+++ utils/convertImg.py
@@ -1,31 +1,25 @@
-"""# conversione di ogni tipo di immagine in jpg per mandarlo a plantnet
+# conversione di ogni tipo di immagine in jpg per mandarlo a plantnet
 from PIL import Image
+import os
 
+def convertJpg(image_path:str, dest_folder:str)->str:
+    """ convert any format of images into jpg, saves them in dest_folder and return its location
 
-def convertJpg(image):
-    '''
-    Accetta string con path dell' immagine salvata in upload
-    converte immagine in jpg
-    salva immagine su cartella static/tmp/conv/
-
-    Parameters:
-    ---
-    image : str
-        Path dell'immagine
-
-    Returns:
-    ---
-    converted_image_path : str
-        Percorso nuova immagine
-    '''
+    :param image_path: path to the source
+    :type image_path: str
+    :param dest_folder: path to folder where to save the image
+    :type dest_folder: str
+    :return: path to the source
+    :rtype:str
+    """ 
     # Importa l'immagine
-    im = Image.open(image)
+    im = Image.open(image_path)
     # Converte in jpg
     rgb_im = im.convert("RGB")
-    # Cambia il path per l'esportazione
-    imageconv = image.replace('upload', 'conv')
-    # Esporta l'immagine
-    rgb_im.save(imageconv + '_conv.jpg')
-    converted_image_path = (imageconv + '_conv.jpg')
+    filename = image_path.split('/')[-1] # name of the image
+    print('filename: ', filename)
+    converted_image_path = os.path.join(dest_folder, filename) + '_conv.jpg'
+    print('conv img path: ', converted_image_path)
+    rgb_im.save(converted_image_path)
+
     return converted_image_path
-"""
